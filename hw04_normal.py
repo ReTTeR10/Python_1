8da4e095e3acdde781b8975b295039c714b551f2
Yegor Mishin <yegor1993@gmail.com>
2018-08-05 23:25:10 +0300

    Решение всех 3х заданий Но 1ую и 2ую надо сделать без re, и в 3ей добавить запись в файл

--- hw04_normal.py
+++ hw04_normal.py
@@ -1,9 +1,10 @@
+__author__ = 'Мишин Егор Олегович'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,15 +21,22 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+print(re.findall('[A-Z]*([a-z]+)[A-Z]', line))
+
+
+
+
+
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
        'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
@@ -45,9 +53,45 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+print(re.findall('[a-z]{2}([A-Z]+)[A-Z]{2}', line_2))
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+import random
+import os
+
+
+# path = os.path.join('C:/', 'Projects/', 'git/', 'data/', 'fruits.txt')
+
+num = [random.randint(0, 9) for _ in range(2500)]
+print(num, '\n', len(num))
+
+num1 = [1, 2, 4, 4, 4, 2, 1, 0, 2, 2, 2, 2, 5, 7, 3, 2, 5, 1]
+
+max = []
+pret = []
+i = 0
+l = 1
+while i!= len(num)-1:
+#while i != len(num1)-1:
+    pret = [num[i]]
+    while num[i] == num[i+1]:
+        l += 1
+        pret.append(num[i])
+        i += 1
+        # print(pret)
+        l = 1
+    #print('2', pret)
+    if len(pret) > len(max):
+        max = pret.copy()
+        pret = []
+    else:
+        pass
+    i += 1
+    pret = []
+print(max)
+#print('1', pret)
