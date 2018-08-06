10efa55ad147eab2e17cd61cac321dc18af0ff44
Yegor Mishin <yegor1993@gmail.com>
2018-08-06 14:25:34 +0300

    Решение 3й и исправления во второй

--- hw04_easy.py
+++ hw04_easy.py
@@ -3,7 +3,7 @@ __author__ = 'Мишин Егор Олегович'
 # Все задачи текущего блока решите с помощью генераторов списков!
 
 # Задание-1:
-# Дан список, заполненный произвольными целыми числами. 
+# Дан список, заполненный произвольными целыми числами.
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
@@ -19,14 +19,18 @@ print('Список 1: ', my_list, '\nСписок 2: ', my_list1)
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
-# fruits1 = [fruit for fruit in ['banana', 'coconut', 'apple', 'orange']]
-# fruits2 = [fruit for fruit in ['kiwi', 'banana', 'mango', 'blackberry', 'watermelon']]
-# print(fruits1)
-# print(fruits2)
-# print(fruit if )
+print('\n Задание №2 - фрукты')
+
+fruits1 = [fruit for fruit in ['banana', 'coconut', 'apple', 'orange']]
+fruits2 = [fruit for fruit in ['kiwi', 'banana', 'mango', 'blackberry', 'watermelon', 'orange']]
+
+print(fruits1)
+print(fruits2)
+fruits3 = [fruit for fruit in fruits1 if fruit in fruits2]
+print('\n В обоих списках присутствуют: ', fruits3)
 
 
-print(round(3.3%3,1))
+
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -34,4 +38,20 @@ print(round(3.3%3,1))
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+import random
+print('\n Задание 3 - произвольные числа')
+
+#num = [random.randint(-100, 100) for _ in range(10)]
+num = [l for l in [3.3, 21, 10, 4, -4, 0, -2.1, 2.3, 2.4]]
+
+print('\n Исходный список: ', num)
+answer1 = [i for i in num if i % 3 == 0]
+answer2 = [i for i in num if i > 0]
+answer3 = [i for i in num if i % 4 != 0]
+
+print('\n', answer1, '\n', answer2, '\n', answer3)
+
+print(3.3 % 3 == 0)
+
 
