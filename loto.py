19f0129f6d02f356d828c91ecec12596d65c83ef
Yegor Mishin <yegor1993@gmail.com>
2018-08-16 10:12:29 +0300

    Лото выполнено

--- loto.py
+++ loto.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+__author__ = 'Мишин Егор Олегович'
 
 """
 == Лото ==
@@ -57,3 +57,85 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+
+
+
+import random
+from sys import exit
+
+
+def sort_to_max(origin_list, start, end):    # сортируем по возрастанию значения в карточке
+    for i in range(end, start, -1):
+        for j in range(start, i):
+            if origin_list[j] > origin_list[j + 1]:
+                origin_list[j], origin_list[j + 1] = origin_list[j + 1], origin_list[j]
+
+
+class Cards:
+    def __init__(self):
+        self.row_count = 3
+        self.column_count = 9
+        self.number_in_row_count = 5
+        self.numbers = random.sample(range(1, 91), 15)
+        self.kegs_count = 15
+        self.distribution_row = list()
+        for i in range(self.row_count):
+            sort_to_max(self.numbers, self.number_in_row_count * i, self.number_in_row_count * (i + 1) - 1)
+            self.distribution_row += random.sample(range(self.column_count), self.number_in_row_count)
+
+    def __str__(self):
+        card = ''
+        num_index = 0
+        for row in range(self.row_count):
+            for col in range(self.column_count):
+                if col in self.distribution_row[self.number_in_row_count * row: self.number_in_row_count * (row + 1)]:
+                    card += ' ' * (2 - len(str(self.numbers[num_index]))) + str(self.numbers[num_index]) + ' '
+                    num_index += 1
+                else:
+                    card += ' ' * 3
+            card += '\n'
+        return card
+
+    def delete_number(self, keg):
+        i = self.numbers.index(keg)
+        self.numbers[i] = '-'
+        self.kegs_count -= 1
+        return True
+
+
+if input("\nКак насчет партейки в Лото? y/n\n") == 'y':
+    player_card = Cards()
+    computer_card = Cards()
+    kegs = random.sample(range(1, 91), 90)
+    step = 0
+    ans = ''
+    while player_card.kegs_count > 0 and computer_card.kegs_count > 0:
+        if ans == 'q':
+             print('Жаль, пока!')
+             exit()
+        print('Tекущий номер |{}| (осталось {})'.format(kegs[step], 90 - step - 1))
+        print('------ Ваша карточка -----')
+        print(player_card)
+        print('-- Карточка компьютера ---')
+        print(computer_card)
+        ans = input('Зачеркнуть цифру? (y/n/q): ')
+        try:
+            concurrence = player_card.delete_number(kegs[step])
+        except ValueError:
+            concurrence = False
+        try:
+            computer_card.delete_number(kegs[step])
+        except ValueError:
+            pass
+        if (ans == 'y' and not concurrence) or (ans == 'n' and concurrence):
+            print('Ошибка! Вы проиграли!')
+            ans = 'q'
+            exit()
+        print()
+        step += 1
+    if player_card.kegs_count == 0:
+        print('Вы выиграли!')
+    elif computer_card.kegs_count == 0:
+        print('Вы проиграли!')
+    elif player_card.kegs_count == 0 and computer_card.kegs_count == 0:
+        print('Ничья!')
